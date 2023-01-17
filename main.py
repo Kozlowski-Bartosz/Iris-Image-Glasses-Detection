12d7e2f66c206c4648bd881f7e565d4650961d0a
Bartosz Kozlowski <kozlowski.bartosz@protonmail.com>
2023-01-17 03:15:50 +0100

    Base edge detection implementation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import reflection_detection as rd
+import edge_detection as ed
 
 from sys import argv
 from sys import exit as sysexit
@@ -13,6 +14,9 @@ resized_img = cv2.resize(img, (320, 240))
 
 #May differ depending on image brightness
 rd.process_image(resized_img, 180)
+edges_image, line_amount = ed.detect_glasses(resized_img)
+cv2.imshow("Edges", edges_image)
+print("Line amount: " + str(line_amount))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
